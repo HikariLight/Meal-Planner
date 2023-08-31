acf30a180fa3afafbfcf9462aef88f054710e423
Dhia M <merzougui.dhia@gmail.com>
2023-08-31 15:53:23 +0200

    Updated layout with footer and navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -11,7 +13,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <div className="">
+                    <div className="h-[3vh]">
+                        <Navbar />
+                    </div>
+
+                    <div className="h-[94vh] overflow-auto">{children}</div>
+
+                    <div className="h-[3vh]">
+                        <Footer />
+                    </div>
+                </div>
+            </body>
         </html>
     )
 }
