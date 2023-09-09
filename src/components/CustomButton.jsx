110489d457e1250c7f769796b6ab9647aef964ed
Dhia M <merzougui.dhia@gmail.com>
2023-09-09 13:24:13 +0200

    Redesigned CustomButton

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -2,7 +2,7 @@ export default function CustomButton({ textValue, clickHandler }) {
     return (
         <button
             type="button"
-            className="rounded-md bg-indigo-600 hover:bg-indigo-800 dark:bg-indigo-500 text-white p-2 my-2"
+            className="rounded-md bg-indigo-700 hover:bg-indigo-800 dark:bg-indigo-500 text-white p-2 my-2"
             onClick={(event) => clickHandler(event)}
         >
             {textValue}
