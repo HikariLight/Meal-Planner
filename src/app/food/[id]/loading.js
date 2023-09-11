d91cd365633e4d846f4671c138e994cd1b49501a
Dhia M <merzougui.dhia@gmail.com>
2023-09-11 14:44:46 +0200

    Improved ingredient info page loading animation

--- src/app/food/[id]/loading.js
+++ src/app/food/[id]/loading.js
@@ -1,9 +1,9 @@
+import { LoadingSpinner } from "@/components"
+
 export default function FoodPageLoading() {
     return (
         <div className="h-full grid place-items-center dark:text-white">
-            <h1 className="text-2xl text-indigo-700 dark:text-indigo-500">
-                Loading...
-            </h1>
+            <LoadingSpinner size={16} />
         </div>
     )
 }
