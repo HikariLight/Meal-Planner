aa7eec5dc74a754d891fb944fb881512bb660d1c
Dhia M <merzougui.dhia@gmail.com>
2023-09-11 14:34:20 +0200

    Added search page

--- src/app/search/IngredientSearchCard.js
+++ src/app/search/IngredientSearchCard.js
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function IngredientSearchCard({ foodData }) {
     return (
-        <div className="w-full">
+        <div className="max-h-48 text-white">
             <Link href={`/food/${foodData.fdcId}`}>
-                <div className="border border-purple-800 rounded p-2 my-2">
+                <div className="border border-indigo-700 dark:border-indigo-500 rounded p-2 my-2">
                     <h1 className="text-xl">{foodData.description}</h1>
                 </div>
             </Link>
