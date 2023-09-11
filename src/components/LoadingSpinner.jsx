b067031c8d373f17e09fe42cbb0906f64b12ffc4
Dhia M <merzougui.dhia@gmail.com>
2023-09-11 14:44:31 +0200

    Added size parameter to LoadingSpinner animation

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,11 @@
-export default function LoadingSpinner() {
+export default function LoadingSpinner({ size }) {
     return (
         <div role="status">
             <svg
                 aria-hidden="true"
-                className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                className={`w-${size ? size : 8} h-${
+                    size ? size : 8
+                } mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`}
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
